400a3e4b774902baeca873be5daf2e3f168482d5
Elijah.chc <eliahcoe004@gmail.com>
2018-06-16 04:18:41 -0400

    Update ListNum.py

--- ListNum.py
+++ ListNum.py
@@ -1,7 +1,10 @@
 # enter a list of numbers and find max and min
 # print sorted numbers
 # created: June 16, 2018 (first version)
+# detail: sec 1 is done on June 16, sec 2 & 3 done before June 16
+# only for exercise
 
+# sec 1: min/ max/ sport w/ numbers list 
 numbers = list()
 
 while(True):
@@ -25,3 +28,30 @@ else:
     print('empty number list')
     
 type(numbers)
+
+# sec 2: not using list for avg  calculation
+total = 0
+count = 0
+
+while(True): # infinite loop until done 
+    inp = input('Enter a number: ')
+    if inp == 'done':
+        break
+    value = float(inp)
+    total = total + value
+    count = count + 1
+    
+average = total / count 
+print('Average: ', average)
+
+# sec 3: using list for avg calculation
+numlist = list()
+while(True):
+    inp = input('Enter a number:')
+    if inp == 'done':
+        break
+    value = float(inp)
+    numlist.append(value)
+
+average = sum(numlist) / len(numlist)
+print('Average: ', average)
